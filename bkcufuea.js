d5f36aa7b4961350aa7c81d5eb4032b9572126f0
rhtzhdls <tndlelwkdls1@gmail.com>
2025-05-29 20:07:49 +0900

    Update bkcufuea.js

--- bkcufuea.js
+++ bkcufuea.js
@@ -2,7 +2,7 @@
     if(window.location.pathname === '/' || window.location.pathname === '/index.html'){
       var logo = document.querySelector('.logo');
       if(logo){
-        logo.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhEsfYFUxh03ABi4k06e3S3B5HC30q9RcUMdrGvVF68cJrZZ8210Ewn4mfWhz09tnfpiFlIaCJXXPb9aLgCZhpH7XaDUqJheqHUqn5ggCGRn9TL_VdoRHVz_rVX2N73oM2GClGlSA5vsG_VubETpQ4vUC1fyQ56XSDKLluWx5csMt6mu6hACeBtklICqZeI/s16000/%ED%99%94%EC%9D%B4%ED%8A%B8%EB%A1%9C%EA%B3%A03.webp";
+        logo.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgxHmC-3dnZf6QV8V0wprFKq1KBBz1RdgbSOTs5UhE9uaqH16ylU4-UFU80RDiJPWj796EpvMKB__Xd3GtQd-rdPdLC_Obyn9s81nZeLCCRTHsHQIkxhpEoVyk2mmzVYfTSxiFpsggWtSEbeAGB8ZzbWUF-8bpe_peSy8EMHBIsdIfDARn_cNOwh22TZPJI/s16000/%ED%99%94%EC%9D%B4%ED%8A%B8%EB%A1%9C%EA%B3%A03.webp";
       }
       var vds = document.getElementById('vds');
       if(vds){
